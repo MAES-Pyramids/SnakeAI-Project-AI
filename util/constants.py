1a95ccb3a50c14fb44182ca03e8ffb4a6c96958c
Muhammed Salman <mahmadslman@gmail.com>
2023-03-05 20:03:26 +0200

    Add basic logic

---
+++ util/constants.py
@@ -0,0 +1,7 @@
+class Constants:
+    WIDTH = 800
+    HEIGHT = 600
+    PIXEL_SIZE = (1, 1)
+    GRID_SIZE = (WIDTH // PIXEL_SIZE[0], HEIGHT // PIXEL_SIZE[1])
+    FPS = 60
+    WINDOW_TITLE = "Snake AI - Best game in the world. Powered by AI"
