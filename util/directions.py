aacf13b510fbbf90a5ce233985faf113a6d2bfa8
Ahmed Mohsen <ahmed25004@hotmail.com>
2023-03-09 15:31:27 +0200

    Refactoring structure

--- util/directions.py
+++ util/directions.py
@@ -1,13 +1,14 @@
 import sys
 sys.path.append('util')
 from point import Point
+from util.point import Point
+
+
 class Direction:
-    UP = Point(0,-1)
+    UP = Point(0, -1)
     DOWN = Point(0, 1)
     RIGHT = Point(1, 0)
     LEFT = Point(-1, 0)
 
-
-    def __eq__(self, __o: object) -> bool:
-        return self == __o
-    
+    def __eq__(self, other: object) -> bool:
+        return self == other
