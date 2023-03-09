d54cc0b70bd4b35612b50d5bac48fe8faad52028
Ahmed Mohsen <ahmed25004@hotmail.com>
2023-03-09 23:17:39 +0200

    adding main file structure

---
+++ sprites/wall.py
@@ -0,0 +1,13 @@
+from util.point import Point
+from util.constants import CONSTANTS
+import pygame
+
+
+class Wall(pygame.sprite.Sprite):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.image = pygame.transform.smoothscale(pygame.image.load(
+            r"assets\images\Wall.jpg"), CONSTANTS.PIXEL_SIZE)
+        self.rect = self.image.get_rect()
+        self.position = Point(0, 0)
