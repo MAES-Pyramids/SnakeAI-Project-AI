2e28d8537eb1e5da8ae351dff2dcba91aa9d9016
Ahmed Mohsen <ahmed25004@hotmail.com>
2023-03-09 15:32:07 +0200

    Refactoring structure

--- sprites/food.py
+++ sprites/food.py
@@ -5,19 +5,18 @@ from util.constants import CONSTANTS
 import pygame
 import random
 
+
 class Food(pygame.sprite.Sprite):
-    
+
     def __init__(self) -> None:
         super().__init__()
         self.image = pygame.Surface(CONSTANTS.PIXEL_SIZE)
         self.rect = self.image.get_rect()
         self.image.fill('Red')
-        self.position = Point(0,0)
+        self.position = Point(0, 0)
         self.spawn()
 
-
-
-    def spawn(self):
+    def spawn(self) -> None:
         self.position = Point(
             random.randint(0, CONSTANTS.GRID_SIZE[0]-1),
             random.randint(0, CONSTANTS.GRID_SIZE[1]-1),
