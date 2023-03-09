ebd8daccd5d68f4616c1b067d4300b7dd237ce59
Ahmed Mohsen <ahmed25004@hotmail.com>
2023-03-09 18:48:05 +0200

    Revert "Adding fixed update function"
    This reverts commit e5de8f86baf2cfa4183fcda293c42ff7f63b6267.

--- util/constants.py
+++ util/constants.py
@@ -2,7 +2,7 @@ class CONSTANTS:
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
     WINDOW_TITLE = "Snake AI - Best game in the world. Powered by AI"
-    PIXEL_SIZE = [30, 30]
+    PIXEL_SIZE = [20, 20]
     GRID_SIZE = (WINDOW_WIDTH // PIXEL_SIZE[0], WINDOW_HEIGHT // PIXEL_SIZE[1])
     FPS = 60
     TIME_STEP = 100
