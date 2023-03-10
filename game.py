a9a947dd0db0cb114b4c3f7dcd6152d8708b2eb4
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-03-10 21:48:58 +0200

    applying sounds effect and changing speed feature with a new background and score label

--- game.py
+++ game.py
@@ -9,18 +9,24 @@ from sprites.snake import Snake, Segment
 
 class Game:
     pygame.init()
+    pygame.mixer.init()
     pygame.display.set_caption(CONSTANTS.WINDOW_TITLE)
+    pygame.display.set_icon(pygame.image.load(r"assets\images\snake.png"))
+
 
     def __init__(self) -> None:
         self.game_window = pygame.display.set_mode(
             (CONSTANTS.WINDOW_WIDTH, CONSTANTS.WINDOW_HEIGHT))
-        self.events = []
-        self.steps = []
-        self.snake = Snake()
-        self.food = Food()
-        self.GAME_OVER = False
         self.grid = np.ndarray(CONSTANTS.GRID_SIZE, object)
         self.clock = pygame.time.Clock()
+        self.snake = Snake()
+        self.food = Food()
+        self.events = []
+        self.steps = []
+        self.background = pygame.image.load(r"assets\images\background.jpg")
+
+        self.GAME_OVER = False
+        self.play_music()
         self.old_tick = 0
 
     # game loop
@@ -38,12 +44,25 @@ class Game:
     def update(self) -> None:
         self.clock.tick(CONSTANTS.FPS)
         self.game_window.fill('black')
+        self.game_window.blit(self.background, (0, 0))
         self.fill_grid()
         self.draw_grid()
+        self.display_score()
         self._handle_input()
+
+        # Check if snake head is on the same position as food 
         if self.snake.head.position == self.food.position:
+            pygame.mixer.Sound(r"assets\sounds\eat.mp3").play()
             self.snake.grow()
             self.food.spawn()
+
+        # Check if snake head is on the same position as any of the body segments
+        if self.snake.length() >= 4 and self.snake.head.position in [seg.position for seg in self.snake.body[:-1]]: 
+            pygame.mixer.Sound(r"assets\sounds\crash.mp3").play()
+            self.GAME_OVER = True
+
+
+        
         pygame.display.update()
 
     def fixed_update(self):
@@ -54,35 +73,50 @@ class Game:
                 self.snake.change_direction(self.steps.pop(0))
             self.snake.move()
             self.old_tick = ticks
+    # -------------Display Score on the screen--------------- #
+    def display_score(self):
+        font = pygame.font.SysFont('comicsans', 20 , True)
+        text = font.render(f"Score: {self.snake.length() - 1}", True, (255, 255, 255))
+        self.game_window.blit(text, (10, 10))
 
+    # ------Create a 2D array to store the game objects------ #
     def fill_grid(self):
         self.grid = [[None for col in range(CONSTANTS.GRID_SIZE[1])] for row in range(
             CONSTANTS.GRID_SIZE[0])]
         for seg in self.snake.body:
             self.grid[seg.position.x][seg.position.y] = seg
         self.grid[self.food.position.x][self.food.position.y] = self.food
-
+    # ----------------- Draw the game objects ------------------ #
     def draw_grid(self):
         for row in self.grid:
             for cell in row:
                 if cell != None:
                     self.game_window.blit(
                         cell.image, (cell.position.x*CONSTANTS.PIXEL_SIZE[0], cell.position.y*CONSTANTS.PIXEL_SIZE[1]))
-
+                    
+    # ------------------ Handle user input ------------------ #
     def _handle_input(self):
         for event in self.events:
             if event.type == pygame.KEYDOWN:
+                CONSTANTS.TIME_STEP /= 1.8
+
                 action = event.key
                 match action:
-                    case pygame.K_w:
+                    case pygame.K_UP:
                         self.steps.append(Direction.UP)
-                    case pygame.K_s:
+                    case pygame.K_DOWN:
                         self.steps.append(Direction.DOWN)
-                    case pygame.K_a:
+                    case pygame.K_LEFT:
                         self.steps.append(Direction.LEFT)
-                    case pygame.K_d:
+                    case pygame.K_RIGHT:
                         self.steps.append(Direction.RIGHT)
 
+            if event.type == pygame.KEYUP:
+                CONSTANTS.TIME_STEP = CONSTANTS.default_STEP
+    # -----------------Play Background music ---------------- #
+    def play_music(self):
+        pygame.mixer.music.load(r"assets\sounds\background.mp3")
+        pygame.mixer.music.play()
 
 if __name__ == '__main__':
     Game().run()
--- util/constants.py
+++ util/constants.py
@@ -1,8 +1,13 @@
 class CONSTANTS:
+    WINDOW_TITLE = "Snake AI - Best game in the world. Powered by AI"
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
-    WINDOW_TITLE = "Snake AI - Best game in the world. Powered by AI"
+
     PIXEL_SIZE = [30, 30]
     GRID_SIZE = (WINDOW_WIDTH // PIXEL_SIZE[0], WINDOW_HEIGHT // PIXEL_SIZE[1])
+
+    # Time step for fixed update
+    default_STEP =140
+    TIME_STEP = default_STEP
+
     FPS = 60
-    TIME_STEP = 100
